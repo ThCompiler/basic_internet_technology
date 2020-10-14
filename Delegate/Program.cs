901e56164368460d23ab5c91d6e70f8096e194a1
TheCompiler <vet_v2002@mail,ru>
2020-10-14 22:25:53 +0300

    add sixth lab

---
+++ Delegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IT6._1 {
+    class Program {
+
+
+        public delegate T Operation<T> (T a, T b);
+
+        static public int Sum(int a, int b) {
+            return a + b;
+        }
+
+        static public int DoOperation(int a, int b, Operation<int> operation) {
+            return operation(a, b);
+        }
+
+        static public int DoOperationStandart(int a, int b, Func<int, int, int> operation) {
+            return operation(a, b);
+        } 
+
+
+        static void Main(string[] args) {
+            int a = 1;
+            int b = 30;
+
+            Console.WriteLine("Мой делегат: Делегат: " + DoOperation(a, b, Sum).ToString() + "\n" +
+                                    "Лямбда: " + DoOperation(a, b, (int a, int b) => a + b).ToString());
+
+            Console.WriteLine("Func: Делегат: " + DoOperationStandart(a, b, Sum).ToString() + "\n" +
+                                    "Лямбда: " + DoOperationStandart(a, b, (int a, int b) => a + b).ToString());
+
+
+        }
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Reflection;
+
+namespace IT6._2 {
+    
+    public class StringAttribute: Attribute {}
+    class Student {
+        public Student(string name, int age) {
+            Name = name;
+            Age = age;
+        }
+
+        [String]
+        public string Name { get; set; } = "";
+
+        public int Age { get; set; } = 0;
+
+        public void Print() {
+            Console.WriteLine("Имя студента: " + Name + ", возраст студента: " + Age);
+        }
+    }
+    
+    class Program {
+        static void Main(string[] args) {
+            Type student = Type.GetType("IT6._2.Student");
+
+            Console.WriteLine("Конструкторы:");
+
+            foreach(var constructor in student.GetConstructors()) {
+                Console.WriteLine(constructor.Name);
+            }
+
+            Console.WriteLine("\nПоля:");
+
+            foreach (var constructor in student.GetFields(/*BindingFlags.NonPublic | BindingFlags.Instance*/)) {
+                Console.WriteLine(constructor.Name);
+            }
+
+            Console.WriteLine("\nМетоды");
+
+            foreach (var constructor in student.GetMethods()) {
+                Console.WriteLine(constructor.Name);
+            }
+
+            Console.WriteLine("\nСвойства");
+
+            foreach (var constructor in student.GetProperties()) {
+                Console.WriteLine(constructor.Name);
+            }
+
+            Console.WriteLine("\nЧлены класса с атрибутом String: ");
+
+            foreach (var member in student.GetMembers()) {
+                foreach (var attribute in member.GetCustomAttributes()) {
+                    if (attribute is StringAttribute) {
+                        Console.WriteLine(member.Name);
+                    }
+                }
+            }
+
+            Console.WriteLine("\n");
+
+            MethodInfo method = student.GetMethod("Print", new Type[] {});
+
+            Student student_class = new Student ( "Ivan", 25 ); 
+            method.Invoke(student_class, new Object[] { });
+        }
+    }
+}
